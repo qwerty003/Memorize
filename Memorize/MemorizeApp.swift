4d2b65373444a2ff65de98509e9274f4c440df47
qwerty003 <abhijeetcatches33@gmail.com>
2024-08-19 13:36:36 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Abhijeet Kumar  on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
